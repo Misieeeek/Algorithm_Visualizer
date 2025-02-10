ec2a4341be99386d41359be237a221408f27c24d
Misieeeek <122481498+Misieeeek@users.noreply.github.com>
2025-02-10 18:16:14 +0100

    Create options.h

---
+++ include/options.h
@@ -0,0 +1,29 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "main_window.h"
+
+#define NUMBER_OF_OPTIONS 3
+
+class Options : public Screen
+{
+private:
+	int selected_options_index;
+	sf::Text options_options[NUMBER_OF_OPTIONS];
+	sf::Font open_sans;
+	int selected_option;
+public:
+	Options();
+	~Options();
+
+	void draw(sf::RenderWindow& window) override;
+	void move_up() override;
+	void move_down() override;
+	int pressed() override { return selected_option; }
+	void change_option(int selected) override;
+};
+
+#endif
