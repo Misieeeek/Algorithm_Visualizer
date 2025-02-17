f0d64c74fdcf84cf56648044ecf0dbe69694283c
Pawel Mis <kacper.mis02@gmail.com>
2025-02-17 14:21:15 +0100

    Optimized and centralized drop down function to just one

--- include/visualizer.h
+++ include/visualizer.h
@@ -6,6 +6,8 @@
 #include "main_window.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <string>
+#include <vector>
 
 #define NUMBER_OF_ALGORITHMS 8
 #define NUMBER_OF_SORT_ALGO 6
@@ -26,26 +28,48 @@ private:
   sf::Font open_sans;
   int selected_algorithm;
 
+  std::vector<sf::Text> list_algo;
+
   // DROP DOWN MENU FOR SORTING ALGORITHMS
   sf::Text list_algo_sort[NUMBER_OF_SORT_ALGO];
+  std::vector<std::string> algo_sort = {"Insertion Sort", "Selection Sort",
+                                        "Merge Sort",     "Bubble Sort",
+                                        "Heap Sort",      "Quick Sort"};
 
   // DROP DOWN MENU FOR SEARCH ALGORITHMS
   sf::Text list_algo_search[NUMBER_OF_SEARCH_ALGO];
+  std::vector<std::string> algo_search = {"Linear Search", "Binary Search"};
 
   // DROP DOWN MENU FOR DS ALGORITHMS
   sf::Text list_algo_ds[NUMBER_OF_DS_ALGO];
+  std::vector<std::string> algo_ds = {"Stack",
+                                      "Queue",
+                                      "Linked List",
+                                      "Hash Table",
+                                      "Binary Search Tree",
+                                      "Red-Black Tree",
+                                      "AVL Tree",
+                                      "Treaps"};
 
   // DROP DOWN MENU FOR DYNAMIC PROGRAMMING ALGORITHMS
   sf::Text list_algo_dynamic[NUMBER_OF_DYNAMIC_ALGO];
+  std::vector<std::string> algo_dynamic = {"Cut Rod", "Cut Rod (Memoization)",
+                                           "Cut Rod (Tabulation)",
+                                           "Longest Common Subsequence"};
 
   // DROP DOWN MENU FOR GREEDY ALGORITHMS
   sf::Text list_algo_greedy[NUMBER_OF_GREEDY_ALGO];
+  std::vector<std::string> algo_greedy = {"Huffman Coding",
+                                          "Activity Selector"};
 
   // DROP DOWN MENU FOR ADVANCED DATA STRUCTURES ALGORITHMS
   sf::Text list_algo_advancedds[NUMBER_OF_ADVANCEDDS_ALGO];
+  std::vector<std::string> algo_advancedds = {"B-Tree", "Fibonacci Heap",
+                                              "Van Emde Boas Tree"};
 
   // DROP DOWN MENU FOR GRAPH ALGORITHMS
   sf::Text list_algo_graph[NUMBER_OF_GRAPH_ALGO];
+  std::vector<std::string> algo_graph = {"1", "2", "3"};
 
   // TEXT STYLES
   int category_font_size = 35;
@@ -73,13 +97,12 @@ public:
   void change_option(int selected) override;
 
   void drop_down(int option) override;
-  void sorting_algo_list();
-  void searching_algo_list();
-  void ds_algo_list();
-  void dynamic_algo_list();
-  void greedy_algo_list();
-  void advancedds_algo_list();
-  void graph_algo_list();
+
+  void general_algo_list(int number_of_categories,
+                         std::vector<std::string> list_of_algo, int end_iter,
+                         int add_val_pos_x_drop_down,
+                         int add_val_pos_x_categories_before,
+                         int adda_val_pos_x_categories_afer);
 
 private:
   Algocat ac;
--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -62,13 +62,11 @@ void Visualizer::move_down() {
 
 void Visualizer::draw(sf::RenderWindow &window) {
   opend = true;
-  // main_active = false;
-
   switch (static_cast<int>(ac)) {
   case 0: // SORTING
     window.draw(list_algorithms[0]);
     for (int i = 0; i < NUMBER_OF_SORT_ALGO; i++)
-      window.draw(list_algo_sort[i]);
+      window.draw(list_algo[i]);
     for (int i = 1; i < NUMBER_OF_ALGORITHMS; i++)
       window.draw(list_algorithms[i]);
     break;
@@ -76,7 +74,7 @@ void Visualizer::draw(sf::RenderWindow &window) {
     for (int i = 0; i < 2; i++)
       window.draw(list_algorithms[i]);
     for (int i = 0; i < NUMBER_OF_SEARCH_ALGO; i++)
-      window.draw(list_algo_search[i]);
+      window.draw(list_algo[i]);
     for (int i = 2; i < NUMBER_OF_ALGORITHMS; i++)
       window.draw(list_algorithms[i]);
     break;
@@ -84,7 +82,7 @@ void Visualizer::draw(sf::RenderWindow &window) {
     for (int i = 0; i < 3; i++)
       window.draw(list_algorithms[i]);
     for (int i = 0; i < NUMBER_OF_DS_ALGO; i++)
-      window.draw(list_algo_ds[i]);
+      window.draw(list_algo[i]);
     for (int i = 3; i < NUMBER_OF_ALGORITHMS; i++)
       window.draw(list_algorithms[i]);
     break;
@@ -92,7 +90,7 @@ void Visualizer::draw(sf::RenderWindow &window) {
     for (int i = 0; i < 4; i++)
       window.draw(list_algorithms[i]);
     for (int i = 0; i < NUMBER_OF_DYNAMIC_ALGO; i++)
-      window.draw(list_algo_dynamic[i]);
+      window.draw(list_algo[i]);
     for (int i = 4; i < NUMBER_OF_ALGORITHMS; i++)
       window.draw(list_algorithms[i]);
     break;
@@ -100,7 +98,7 @@ void Visualizer::draw(sf::RenderWindow &window) {
     for (int i = 0; i < 5; i++)
       window.draw(list_algorithms[i]);
     for (int i = 0; i < NUMBER_OF_GREEDY_ALGO; i++)
-      window.draw(list_algo_greedy[i]);
+      window.draw(list_algo[i]);
     for (int i = 5; i < NUMBER_OF_ALGORITHMS; i++)
       window.draw(list_algorithms[i]);
     break;
@@ -108,7 +106,7 @@ void Visualizer::draw(sf::RenderWindow &window) {
     for (int i = 0; i < 6; i++)
       window.draw(list_algorithms[i]);
     for (int i = 0; i < NUMBER_OF_ADVANCEDDS_ALGO; i++)
-      window.draw(list_algo_advancedds[i]);
+      window.draw(list_algo[i]);
     for (int i = 6; i < NUMBER_OF_ALGORITHMS; i++)
       window.draw(list_algorithms[i]);
     break;
@@ -116,7 +114,7 @@ void Visualizer::draw(sf::RenderWindow &window) {
     for (int i = 0; i < 7; i++)
       window.draw(list_algorithms[i]);
     for (int i = 0; i < NUMBER_OF_GRAPH_ALGO; i++)
-      window.draw(list_algo_graph[i]);
+      window.draw(list_algo[i]);
     for (int i = 7; i < NUMBER_OF_ALGORITHMS; i++)
       window.draw(list_algorithms[i]);
     break;
@@ -218,31 +216,33 @@ void Visualizer::drop_down(int option) {
   switch (option) {
   case 0:
     ac = Algocat::SORTING;
-    sorting_algo_list();
+    general_algo_list(NUMBER_OF_SORT_ALGO, algo_sort, 1, 100, 50, 260);
+    // sorting_algo_list();
     break;
   case 1:
     ac = Algocat::SEARCHING;
-    searching_algo_list();
+    general_algo_list(NUMBER_OF_SEARCH_ALGO, algo_search, 2, 250, 150, 220);
     break;
   case 2:
     ac = Algocat::DS;
-    ds_algo_list();
+    general_algo_list(NUMBER_OF_DS_ALGO, algo_ds, 3, 200, 50, 320);
     break;
   case 3:
     ac = Algocat::DYNAMIC;
-    dynamic_algo_list();
+    general_algo_list(NUMBER_OF_DYNAMIC_ALGO, algo_dynamic, 4, 300, 100, 230);
     break;
   case 4:
     ac = Algocat::GREEDY;
-    greedy_algo_list();
+    general_algo_list(NUMBER_OF_GREEDY_ALGO, algo_greedy, 5, 390, 140, 210);
     break;
   case 5:
     ac = Algocat::ADVANCEDDS;
-    advancedds_algo_list();
+    general_algo_list(NUMBER_OF_ADVANCEDDS_ALGO, algo_advancedds, 6, 400, 100,
+                      210);
     break;
   case 6:
     ac = Algocat::GRAPH;
-    graph_algo_list();
+    general_algo_list(NUMBER_OF_GRAPH_ALGO, algo_graph, 7, 445, 100, 185);
     break;
   case 7:
     ac = Algocat::NONE;
@@ -250,143 +250,25 @@ void Visualizer::drop_down(int option) {
   }
 }
 
-void Visualizer::sorting_algo_list() {
-  std::string algo_sort[] = {"Insertion Sort", "Selection Sort", "Merge Sort",
-                             "Bubble Sort",    "Heap Sort",      "Quick Sort"};
-
-  for (int i = 0; i < NUMBER_OF_SORT_ALGO; i++) {
-    list_algo_sort[i].setFont(open_sans);
-    list_algo_sort[i].setFillColor(sf::Color::White);
-    list_algo_sort[i].setCharacterSize(drop_down_item_font_size);
-    list_algo_sort[i].setStyle(sf::Text::Bold);
-    list_algo_sort[i].setPosition(100, 35 * i + 100);
-    list_algo_sort[i].setString(algo_sort[i]);
+void Visualizer::general_algo_list(int number_of_categories,
+                                   std::vector<std::string> list_of_algo,
+                                   int end_iter, int add_val_pos_x_drop_down,
+                                   int add_val_pos_x_categories_before,
+                                   int add_val_pos_x_categories_after) {
+  list_algo.resize(number_of_categories);
+  for (int i = 0; i < number_of_categories; i++) {
+    list_algo[i].setFont(open_sans);
+    list_algo[i].setFillColor(sf::Color::White);
+    list_algo[i].setCharacterSize(drop_down_item_font_size);
+    list_algo[i].setStyle(sf::Text::Bold);
+    list_algo[i].setPosition(100, 35 * i + add_val_pos_x_drop_down);
+    list_algo[i].setString(list_of_algo[i]);
   }
 
-  list_algorithms[0].setPosition(50, 50);
-  for (int i = 1; i < NUMBER_OF_ALGORITHMS; i++) {
-    list_algorithms[i].setPosition(50, 50 * i + 260);
-  }
-}
-
-void Visualizer::searching_algo_list() {
-  std::string algo_search[] = {"Linear Search", "Binary Search"};
-
-  for (int i = 0; i < NUMBER_OF_SEARCH_ALGO; i++) {
-    list_algo_search[i].setFont(open_sans);
-    list_algo_search[i].setFillColor(sf::Color::White);
-    list_algo_search[i].setCharacterSize(drop_down_item_font_size);
-    list_algo_search[i].setStyle(sf::Text::Bold);
-    list_algo_search[i].setPosition(100, 35 * i + 250);
-    list_algo_search[i].setString(algo_search[i]);
-  }
-
-  for (int i = 2; i < NUMBER_OF_ALGORITHMS; i++) {
-    list_algorithms[i].setPosition(50, 50 * i + 220);
-  }
-}
-
-void Visualizer::ds_algo_list() {
-  std::string algo_ds[] = {"Stack",
-                           "Queue",
-                           "Linked List",
-                           "Hash Table",
-                           "Binary Search Tree",
-                           "Red-Black Tree",
-                           "AVL Tree",
-                           "Treaps"};
-
-  for (int i = 0; i < NUMBER_OF_DS_ALGO; i++) {
-    list_algo_ds[i].setFont(open_sans);
-    list_algo_ds[i].setFillColor(sf::Color::White);
-    list_algo_ds[i].setCharacterSize(drop_down_item_font_size);
-    list_algo_ds[i].setStyle(sf::Text::Bold);
-    list_algo_ds[i].setPosition(100, 35 * i + 200);
-    list_algo_ds[i].setString(algo_ds[i]);
-  }
-
-  for (int i = 0; i < 3; i++)
-    list_algorithms[i].setPosition(50, 50 * i + 50);
-  for (int i = 3; i < NUMBER_OF_ALGORITHMS; i++) {
-    list_algorithms[i].setPosition(50, 50 * i + 320);
-  }
-}
-
-void Visualizer::dynamic_algo_list() {
-  std::string algo_dynamic[] = {"Cut Rod", "Cut Rod (Memoization)",
-                                "Cut Rod (Tabulation)",
-                                "Longest Common Subsequence"};
-
-  for (int i = 0; i < NUMBER_OF_DYNAMIC_ALGO; i++) {
-    list_algo_dynamic[i].setFont(open_sans);
-    list_algo_dynamic[i].setFillColor(sf::Color::White);
-    list_algo_dynamic[i].setCharacterSize(drop_down_item_font_size);
-    list_algo_dynamic[i].setStyle(sf::Text::Bold);
-    list_algo_dynamic[i].setPosition(100, 35 * i + 300);
-    list_algo_dynamic[i].setString(algo_dynamic[i]);
-  }
-
-  for (int i = 0; i < 4; i++)
-    list_algorithms[i].setPosition(50, 50 * i + 100);
-  for (int i = 4; i < NUMBER_OF_ALGORITHMS; i++) {
-    list_algorithms[i].setPosition(50, 50 * i + 230);
-  }
-}
-
-void Visualizer::greedy_algo_list() {
-  std::string algo_greedy[] = {"Huffman Coding", "Activity Selector"};
-
-  for (int i = 0; i < NUMBER_OF_GREEDY_ALGO; i++) {
-    list_algo_greedy[i].setFont(open_sans);
-    list_algo_greedy[i].setFillColor(sf::Color::White);
-    list_algo_greedy[i].setCharacterSize(drop_down_item_font_size);
-    list_algo_greedy[i].setStyle(sf::Text::Bold);
-    list_algo_greedy[i].setPosition(100, 35 * i + 390);
-    list_algo_greedy[i].setString(algo_greedy[i]);
-  }
-
-  for (int i = 0; i < 5; i++)
-    list_algorithms[i].setPosition(50, 50 * i + 140);
-  for (int i = 5; i < NUMBER_OF_ALGORITHMS; i++) {
-    list_algorithms[i].setPosition(50, 50 * i + 210);
-  }
-}
-
-void Visualizer::advancedds_algo_list() {
-  std::string algo_advancedds[] = {"B-Tree", "Fibonacci Heap",
-                                   "Van Emde Boas Tree"};
-
-  for (int i = 0; i < NUMBER_OF_ADVANCEDDS_ALGO; i++) {
-    list_algo_advancedds[i].setFont(open_sans);
-    list_algo_advancedds[i].setFillColor(sf::Color::White);
-    list_algo_advancedds[i].setCharacterSize(drop_down_item_font_size);
-    list_algo_advancedds[i].setStyle(sf::Text::Bold);
-    list_algo_advancedds[i].setPosition(100, 35 * i + 400);
-    list_algo_advancedds[i].setString(algo_advancedds[i]);
-  }
-
-  for (int i = 0; i < 6; i++)
-    list_algorithms[i].setPosition(50, 50 * i + 100);
-  for (int i = 6; i < NUMBER_OF_ALGORITHMS; i++) {
-    list_algorithms[i].setPosition(50, 50 * i + 210);
-  }
-}
-
-void Visualizer::graph_algo_list() {
-  std::string algo_graph[] = {"1", "2", "3"};
-
-  for (int i = 0; i < NUMBER_OF_GRAPH_ALGO; i++) {
-    list_algo_graph[i].setFont(open_sans);
-    list_algo_graph[i].setFillColor(sf::Color::White);
-    list_algo_graph[i].setCharacterSize(drop_down_item_font_size);
-    list_algo_graph[i].setStyle(sf::Text::Bold);
-    list_algo_graph[i].setPosition(100, 35 * i + 445);
-    list_algo_graph[i].setString(algo_graph[i]);
-  }
-
-  for (int i = 0; i < 7; i++)
-    list_algorithms[i].setPosition(50, 50 * i + 100);
-  for (int i = 7; i < NUMBER_OF_ALGORITHMS; i++) {
-    list_algorithms[i].setPosition(50, 50 * i + 185);
+  for (int i = 0; i < end_iter; i++)
+    list_algorithms[i].setPosition(50,
+                                   50 * i + add_val_pos_x_categories_before);
+  for (int i = end_iter; i < NUMBER_OF_ALGORITHMS; i++) {
+    list_algorithms[i].setPosition(50, 50 * i + add_val_pos_x_categories_after);
   }
 }
