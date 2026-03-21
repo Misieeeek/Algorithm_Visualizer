16edd623748141daef28b4107736a2a143c768e7
Misieeeek <kacper.mis02@gmail.com>
2026-03-21 23:10:16 +0100

    Added file for predefined styles and style stuct

--- include/GUI/Common.h
+++ include/GUI/Common.h
@@ -2,4 +2,16 @@ namespace alviz::gui {
 struct Color {
   uint8_t r, g, b, a;
 };
+
+struct ButtonStyle {
+  Color normal = {.r = 50, .g = 50, .b = 50, .a = 255};
+  Color hovered = {.r = 80, .g = 80, .b = 80, .a = 255};
+  Color focused = {.r = 100, .g = 100, .b = 200, .a = 255};
+  Color pressed = {.r = 30, .g = 30, .b = 30, .a = 255};
+  Color text = {.r = 255, .g = 255, .b = 255, .a = 255};
+  Color border = {.r = 150, .g = 150, .b = 150, .a = 255};
+  float borderThickness = 1.F;
+  unsigned int fontSize = 14;
+};
+
 };  // namespace alviz::gui
---
+++ include/GUI/Styles.h
@@ -0,0 +1,5 @@
+#include "GUI/Common.h"
+
+namespace alviz::gui::styles {
+inline ButtonStyle Default() { return {}; };
+};  // namespace alviz::gui::styles
