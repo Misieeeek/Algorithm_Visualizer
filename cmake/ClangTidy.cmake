948f73140cb129b63a177f0793720e5127a7e1cc
Misieeeek <kacper.mis02@gmail.com>
2026-04-23 18:56:54 +0200

    Added clang-tidy

---
+++ cmake/ClangTidy.cmake
@@ -0,0 +1,7 @@
+function(StaticChecker target)
+    find_program(CLANG-TIDY_PATH clang-tidy REQUIRED)
+    set_target_properties(${target}
+        PROPERTIES CXX_CLANG_TIDY
+        "${CLANG-TIDY_PATH};-checks=*"
+    )
+endfunction()
