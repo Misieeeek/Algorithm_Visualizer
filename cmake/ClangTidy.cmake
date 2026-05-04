222ef1dfbb3514602fc636c63fa355614f7c4b9e
Misieeeek <kacper.mis02@gmail.com>
2026-05-04 20:06:21 +0200

    Updated formatting for cmake files

--- cmake/ClangTidy.cmake
+++ cmake/ClangTidy.cmake
@@ -1,7 +1,5 @@
 function(StaticChecker target)
-    find_program(CLANG-TIDY_PATH clang-tidy REQUIRED)
-    set_target_properties(${target}
-        PROPERTIES CXX_CLANG_TIDY
-        "${CLANG-TIDY_PATH};-checks=*"
-    )
+  find_program(CLANG-TIDY_PATH clang-tidy REQUIRED)
+  set_target_properties(${target} PROPERTIES CXX_CLANG_TIDY
+                                             "${CLANG-TIDY_PATH};-checks=*")
 endfunction()
--- cmake/Valgrind.cmake
+++ cmake/Valgrind.cmake
@@ -1,21 +1,19 @@
 function(Memcheck target)
-    if(NOT UNIX OR APPLE)
-        message(STATUS "Skipping Valgrind")
-        return()
-    endif()
-    find_program(VALGRIND_PATH valgrind)
-    if (NOT VALGRIND_PATH)
-        message(WARNING "Valgrind not found, skipping analysis for ${target}")
-        return()
-    endif()
-    add_custom_target(memcheck_${target}
-        COMMAND ${VALGRIND_PATH} --leak-check=full --show-leak-kinds=all
-                #--track-origins=yes
-                --error-exitcode=1
-                $<TARGET_FILE:${target}>
-        DEPENDS ${target}
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-    )
+  if(NOT UNIX OR APPLE)
+    message(STATUS "Skipping Valgrind")
+    return()
+  endif()
+  find_program(VALGRIND_PATH valgrind)
+  if(NOT VALGRIND_PATH)
+    message(WARNING "Valgrind not found, skipping analysis for ${target}")
+    return()
+  endif()
+  add_custom_target(
+    memcheck_${target}
+    COMMAND
+      ${VALGRIND_PATH} --leak-check=full --show-leak-kinds=all
+      # --track-origins=yes
+      --error-exitcode=1 $<TARGET_FILE:${target}>
+    DEPENDS ${target}
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
 endfunction()
-
-
